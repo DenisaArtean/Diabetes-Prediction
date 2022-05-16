8ecd70ada13dd0a3ac8eb18bca5060ed7df0e38f
DenisaArtean <denisa.artean01@e-uvt.ro>
2022-05-16 21:17:48 +0300

    Login back-end + Patients back-end + Tests back-end + Prediction

--- app_config.py
+++ app_config.py
@@ -1,6 +1,7 @@
 
 from flask.app import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
 
 
@@ -14,5 +15,9 @@ app.config['SECRET_KEY'] = os.urandom(12).hex()
 
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
